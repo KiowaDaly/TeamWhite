2c5eed9c1ec6c3cfa52742762d3c744f29f53833
rebeccalobo <rebecca.lobo16@gmail.com>
2018-02-12 03:31:42 +0000

    Update Cluedo_Character.java

--- Cluedo_Character.java
+++ Cluedo_Character.java
@@ -1,16 +1,15 @@
 
-public class Cluedo_Character {
+public class Cluedo_Character { //method used in storing and printing the character names.
 	
 		private String characters_name;
 		
-		public Cluedo_Character(String name) {
+		public Cluedo_Character(String name) { //constructor for the method
 			this.characters_name = name;
 		}
-		@Override
-		public String toString() {
+		
+		public String toString() { //toString method returns character names held within the characters_name String variable
 			return characters_name;
 		}
 
-	}
-
+}
 
