06b5e91ba988678ea84504d962983e97508f4ece
LeonaMc <leona.mc-nulty@ucdconnect.ie>
2018-02-25 04:56:30 +0000

    Update Cluedo.java
    Now allows user to enter the amount of players, user will then have to enter names equivalent to that amount. Still need to set tokens to players.

--- Sprint2 source Code/Cluedo.java
+++ Sprint2 source Code/Cluedo.java
@@ -11,7 +11,12 @@ public class Cluedo {
     	final int[] BoundaryColumn = {17,17,17,17,18,19,20,21,23,16,15,14,14,14,14,14,14,13,12,11,10,9,9,9,9,9,9,8,6,6,6,6,6,6,5,4,3,2,1,0,7,-1,0,1,2,3,4,5,6,7,7,7,7,7,7,6,5,4,3,2,1,0,-1,0,1,2,3,4,5,5,5,5,5,6,7,8,9,10,10,9,8,8,8,8,8,8,9,10,11,12,13,14,15,15,15,15,15,15,14,13,13,14,15,16,17,18,18,18,19,20,21,22,23,24,23,22,21,20,19,18,18,18,18,18,19,20,21,22,23,22,21,20,19,18,17,17,17,18,19,20,21,22,23,24,10,10,10,10,10,10,10,11,12,13,14,14,14,14,14,14,14,13,12,11,22};																																
     	int PlayerPositionsRow[] = {0,0,6,19,24,17};
     	int PlayerPositionsColumn[] = {9,14,23,23,7,0};
-        String command = ui.getCommand(); 
+       
+        String command = ui.getCommand(); //INITIALISED
+        int numberOfPlayers = 0; 
+    	String numberOfPlayersString = null;
+        String player;
+        
         people.add("Plum"); //ADDING PLUM TO THE ARRAY
         
         Token white = tokens.get("White");
@@ -20,16 +25,32 @@ public class Cluedo {
         
          //START COMMAND TO BEGIN THE GAME
            if(command.equalsIgnoreCase("start") ) {
-  	         
-        	//TODO: ASK HOW MANY PLAYERS AND CREATE A FOR LOOP FOR UP TO SIX PLAYERS
-        	
-    		ui.displayString("\n\nPlease enter the name of player one: ");
-    		player1 = ui.getCommand();
-        	ui.displayString(player1);
-     //   	player1 = character1;
-        	player1 = people.get(0);
-        	ui.displayString("\n\nYou are " + people.get(0));
+  	              	
+              ui.displayString("\n\nEnter the number of players: ");
+              numberOfPlayersString = ui.getCommand();
+              numberOfPlayers = Integer.parseInt(numberOfPlayersString); //convert int to string
+            
+              if((numberOfPlayers == 0) || (numberOfPlayers == 1)) {
+              
+                ui.displayString("\n\nThis is a multiplayer game. There must be at least 2 players. Please enter a valid numer of players:\n");
+            	
+             }else if(numberOfPlayers <= 6) {
+            	
+                ui.displayString("There will be " + numberOfPlayers + " players");
+          	  
+              for(int i = 0; i < numberOfPlayers; i++) {
+          		  //BODY
+          	    ui.displayString("\n\nPlease enter a name: \n");
+          	   	player = ui.getCommand();
+           	    ui.displayString(player);
+           	    
+           	 
+             	 
+          	  }} else {
+   		        ui.displayString("Please enter a valid number");
+            } 	
         }
+        
         
         do {
             command = ui.getCommand();
