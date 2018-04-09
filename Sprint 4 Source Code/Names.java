6aeaab66513e11f15bce5ad32ffdbb14efbb1872
rebeccalobo <rebec@137.43.249.99>
2018-04-09 04:06:57 +0100

    fixed: tokens not named correctly, ie. Plum character's roll would cause the blue token to move.

--- Sprint 4 Source Code/Names.java
+++ Sprint 4 Source Code/Names.java
@@ -1,6 +1,6 @@
 public final class Names {
 
-    public final static String[] SUSPECT_NAMES = {"Plum", "White", "Scarlett", "Green", "Mustard", "Peacock"};
+    public final static String[] SUSPECT_NAMES = {"Peacock", "White", "Scarlett", "Green", "Mustard", "Plum"};
     public final static String[] WEAPON_NAMES = {"Rope", "Dagger", "Wrench", "Revolver", "Candlestick", "Lead Pipe"};
     public final static String[] ROOM_NAMES = {"Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library",
             "Study", "Hall", "Lounge", "Dining Room", "Cellar"};
